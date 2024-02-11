8bd4e0b93252e980a24bac57b11cd3e8cf860d2f
kensuke.mit@gmail.com <kensuke.mit@gmail.com>
2024-02-11 15:05:54 +0000

    f1 scenario is in devel

--- simulation_extractions/notebooks/construct_f1_scenario.py
+++ simulation_extractions/notebooks/construct_f1_scenario.py
@@ -96,19 +96,20 @@ seq_bus_stops_deleted = []
 tree_xml = ET.parse(s_path_bus_stop_definition.as_posix())
 root = tree_xml.getroot()
 
-for elem in root:
+iter_bus_stop = root.findall('busStop')
+
+for elem in iter_bus_stop:
     # do something with each element here
     element_name: str = elem.tag
-    if element_name == 'busStop':
-        __bus_stop_attrib = elem.attrib
-        __bus_stop_id = __bus_stop_attrib['id']
-        __bus_stop_lane = __bus_stop_attrib['lane']
-        __edge_id, __ = __bus_stop_lane.split('_')
-        if __edge_id in seq_prohibited_edges:
-            seq_bus_stops_deleted.append(__bus_stop_id)
-            logger.debug(f"bus-stop-id: {__bus_stop_id}, Set invisible.")
-            root.remove(elem)
-        # end if
+    __bus_stop_attrib = elem.attrib
+    __bus_stop_id = __bus_stop_attrib['id']
+    __bus_stop_lane = __bus_stop_attrib['lane']
+    __edge_id, __ = __bus_stop_lane.split('_')
+    
+    if __edge_id in seq_prohibited_edges:
+        seq_bus_stops_deleted.append(__bus_stop_id)
+        logger.debug(f"bus-stop-id: {__bus_stop_id}, Set invisible.")
+        root.remove(elem)
     # end if
 # end for
 
@@ -125,28 +126,28 @@ t_path_bus_route_definition = target_path_route_dir / "most.buses.flows.xml"
 tree_xml = ET.parse(s_path_bus_route_definition.as_posix())
 root = tree_xml.getroot()
 
-for elem in root:
+iter_root_node = root.findall('route')
+
+for elem in iter_root_node:
     # do something with each element here
     element_name: str = elem.tag
-    if element_name == 'route':
-        __route_attrib = elem.attrib
-        __seq_edges: ty.List[str] = __route_attrib['edges'].split()
-        # redundant approach to remove the prohibited edges, but I do this style to keep the route ordering.
-        __seq_edges_updated = [__edge for __edge in __seq_edges if __edge not in seq_prohibited_edges]
-        # logger.debug(f"Bus-No: {__route_attrib['id']}, before-mod-N(edges): {len(__seq_edges)}, updated-N(edges): {len(__seq_edges_updated)}")
-        # updating edge information.
-        __route_attrib['edges'] = " ".join(__seq_edges_updated)
-        
-        # deleting the bus stop.
-        __seq_stop_elem = elem.findall('stop')
-        for _elem_stop in __seq_stop_elem:
-            _stop_id = _elem_stop.attrib['busStop']
-            if _stop_id in seq_bus_stops_deleted:
-                elem.remove(_elem_stop)
-                logger.debug(f"removed: {_elem_stop}")
-            # end if
-        # end for
-    # end if
+    __route_attrib = elem.attrib
+    __seq_edges: ty.List[str] = __route_attrib['edges'].split()
+    # redundant approach to remove the prohibited edges, but I do this style to keep the route ordering.
+    __seq_edges_updated = [__edge for __edge in __seq_edges if __edge not in seq_prohibited_edges]
+    # logger.debug(f"Bus-No: {__route_attrib['id']}, before-mod-N(edges): {len(__seq_edges)}, updated-N(edges): {len(__seq_edges_updated)}")
+    # updating edge information.
+    __route_attrib['edges'] = " ".join(__seq_edges_updated)
+    
+    # deleting the bus stop.
+    __seq_stop_elem = elem.findall('stop')
+    for _elem_stop in __seq_stop_elem:
+        _stop_id = _elem_stop.attrib['busStop']
+        if _stop_id in seq_bus_stops_deleted:
+            elem.remove(_elem_stop)
+            logger.debug(f"removed: {_elem_stop}")
+        # end if
+    # end for
 # end for
 
 tree_xml.write(t_path_bus_route_definition.as_posix())
@@ -168,36 +169,38 @@ root = tree_xml.getroot()
 
 seq_parking_id_unavailable = []
 
-for elem in root:
+iter_parking_area_node = root.findall('parkingArea')
+
+for elem in iter_parking_area_node:
     # do something with each element here
     element_name: str = elem.tag
-    if element_name == 'parkingArea':
-        _p_id = elem.attrib['id']
-        _p_lane_id = elem.attrib['lane']
-        _p_edge_id = _p_lane_id.split('_')[0]
-        # logger.debug(_p_edge_id)
-        # when the parking area is located on the prohibited edge, 
-        if _p_edge_id in seq_prohibited_edges:
-            seq_parking_id_unavailable.append(_p_id)
-            # set the parking capacity to 0.
-            elem.attrib['roadsideCapacity'] = "0"
-            logger.debug(f"parking-id: {_p_id}, Set 0 capacity.")
-        # end if
+    _p_id = elem.attrib['id']
+    _p_lane_id = elem.attrib['lane']
+    _p_edge_id = _p_lane_id.split('_')[0]
+    # logger.debug(_p_edge_id)
+    # when the parking area is located on the prohibited edge, 
+    if _p_edge_id in seq_prohibited_edges:
+        seq_parking_id_unavailable.append(_p_id)
+        # set the parking capacity to 0.
+        elem.attrib['roadsideCapacity'] = "0"
+        logger.debug(f"parking-id: {_p_id}, Set 0 capacity.")
     # end if
+# end for
+
+iter_parking_area_node = root.findall('rerouter')
+for elem in iter_parking_area_node:
     # updating rerouter
-    if element_name == 'rerouter':
-        iter_rerouter_def = elem.findall('interval')
-        for __interval_def in iter_rerouter_def:
-            iter_rerouter_def = __interval_def.findall('parkingAreaReroute')
-            for _elem_park_reroute in iter_rerouter_def:
-                _p_id = _elem_park_reroute.attrib['id']
-                if _p_id in seq_parking_id_unavailable:
-                    _elem_park_reroute.attrib['visible'] = "false"
-                    # logger.debug(f"parking-id: {_p_id}, Set invisible.")
-                # end if
-            # end for
+    iter_rerouter_def = elem.findall('interval')
+    for __interval_def in iter_rerouter_def:
+        iter_rerouter_def = __interval_def.findall('parkingAreaReroute')
+        for _elem_park_reroute in iter_rerouter_def:
+            _p_id = _elem_park_reroute.attrib['id']
+            if _p_id in seq_parking_id_unavailable:
+                _elem_park_reroute.attrib['visible'] = "false"
+                # logger.debug(f"parking-id: {_p_id}, Set invisible.")
+            # end if
         # end for
-    # end if
+    # end for
 # end for
 
 tree_xml.write(t_path_parking_add.as_posix())
@@ -219,30 +222,32 @@ t_path_commercial_route = target_path_route_dir / "most.commercial.rou.xml"
 tree_xml = ET.parse(s_path_commercial_route.as_posix())
 root = tree_xml.getroot()
 
-for elem in root:
+iter_vehicle_node = root.findall('vehicle')
+
+for elem in iter_vehicle_node:
     # do something with each element here
     element_name: str = elem.tag
-    if element_name == 'vehicle':
-        _v_id = elem.attrib['id']
-        
-        elem_route = elem.find('route')
-        __route_attrib = elem_route.attrib
-        
-        __seq_edges: ty.List[str] = __route_attrib['edges'].split()
-        # redundant approach to remove the prohibited edges, but I do this style to keep the route ordering.
-        __seq_edges_updated = [__edge for __edge in __seq_edges if __edge not in seq_prohibited_edges]
-        # logger.debug(f"No: {_v_id}, before-mod-N(edges): {len(__seq_edges)}, updated-N(edges): {len(__seq_edges_updated)}")
-        # updating edge information.
-        __route_attrib['edges'] = " ".join(__seq_edges_updated)
-        
-        # updating "stop" element if the parking lot is located on the prohibited edge.
-        # <stop parkingArea="1151" until="45004" />
-        elem_stop = elem.find('stop')
-        if elem_stop is not None:
-            _p_id = elem_stop.attrib['parkingArea']
-            if _p_id in seq_parking_id_unavailable:
-                elem.remove(elem_stop)
-                logger.debug(f"removed: {elem_stop}")
+    _v_id = elem.attrib['id']
+    
+    elem_route = elem.find('route')
+    __route_attrib = elem_route.attrib
+    
+    __seq_edges: ty.List[str] = __route_attrib['edges'].split()
+    # redundant approach to remove the prohibited edges, but I do this style to keep the route ordering.
+    __seq_edges_updated = [__edge for __edge in __seq_edges if __edge not in seq_prohibited_edges]
+    # logger.debug(f"No: {_v_id}, before-mod-N(edges): {len(__seq_edges)}, updated-N(edges): {len(__seq_edges_updated)}")
+    # updating edge information.
+    __route_attrib['edges'] = " ".join(__seq_edges_updated)
+    
+    # updating "stop" element if the parking lot is located on the prohibited edge.
+    # <stop parkingArea="1151" until="45004" />
+    elem_stop = elem.find('stop')
+    if elem_stop is not None:
+        _p_id = elem_stop.attrib['parkingArea']
+        if _p_id in seq_parking_id_unavailable:
+            elem.remove(elem_stop)
+            logger.debug(f"removed: {elem_stop}")
+        # end if
     # end if
 # end for
 
@@ -259,6 +264,8 @@ assert s_path_pedestrial_route.exists(), f"not found: {s_path_pedestrial_route}"
 t_path_pedestrial_route = target_path_route_dir / "most.pedestrian.rou.xml"
 
 # %%
+
+# example of pedestrian route
 """
     <person id="pedestrian_1-1-pt_7251" type="pedestrian" depart="18001">
         <walk edges="-152836#4 -152836#5 153171#1 153151 152870#2 153160 -152969#1 -152969#0" busStop="131101"/>
@@ -279,78 +286,97 @@ t_path_pedestrial_route = target_path_route_dir / "most.pedestrian.rou.xml"
 tree_xml = ET.parse(s_path_pedestrial_route.as_posix())
 root = tree_xml.getroot()
 
-for elem in root:
+iter_person_node = root.findall('person')
+
+for elem in iter_person_node:
     # do something with each element here
     element_name: str = elem.tag
+    # when True, then I delete the agent.
+    is_delete_agent = False
     # process for person tag
-    if element_name == 'person':
-        _id_pedestrian = elem.attrib['id']
-        # updating walk route information
-        elem_walk_def = elem.findall('walk')
-        for _elem_walk in elem_walk_def:
-            # deleting the invalid edges from the route.
-            _seq_edges = _elem_walk.attrib['edges'].split()
-            _seq_edges_updated = [_edge for _edge in _seq_edges if _edge not in seq_prohibited_edges]
-            _elem_walk.attrib['edges'] = " ".join(_seq_edges_updated)
-            if len(_seq_edges_updated) < len(_seq_edges):
-                logger.debug(f"pedestrian: {elem.attrib['id']}, before-mod-N(edges): {len(_seq_edges)}, updated-N(edges): {len(_seq_edges_updated)}")
-            # end if
-            
-            # if deleted bus-stops is in the route, I delete the bus-stop.
-            if 'busStop' in _elem_walk.attrib and _elem_walk.attrib['busStop'] in seq_bus_stops_deleted:
-                del _elem_walk.attrib['busStop']
-            # end if
-        # end for
-        
-        # check ride information.
-        # If this pedestrian requires a ride that passes through the prohibited edges, I delete the pedestrian.
-        elem_ride_def = elem.findall('ride')
-        for _elem_ride in elem_ride_def:
-            _ride_edge_from = ''
-            _ride_edge_to = ''
-            if 'from' in _elem_ride.attrib:
-                _ride_edge_from = _elem_ride.attrib['from']
-            # end if
-            if 'to' in _elem_ride.attrib:
-                _ride_edge_to = _elem_ride.attrib['to']
-            # end if
-            if _ride_edge_from in seq_prohibited_edges or _ride_edge_to in seq_prohibited_edges:
-                # elem.remove(_elem_ride)
-                root.remove(elem)
-                logger.debug(f"removed agent: {_id_pedestrian}")
-            # end if
-            
-            # bus stop.
-            # If this pedestrian requires a ride that passes through the prohibited edges, I delete the pedestrian.
-            if 'busStop' in _elem_ride.attrib and _elem_ride.attrib['busStop'] in seq_bus_stops_deleted:
-                root.remove(elem)
-                logger.debug(f"removed agent: {_id_pedestrian}")
-            # end if
-        # end for
-    # end if
-    
-    if element_name == 'vehicle':
-        elem_route = elem.find('route')
-        assert elem_route is not None, f"not found: route"
-        __route_attrib = elem_route.attrib
-        
-        __seq_edges: ty.List[str] = __route_attrib['edges'].split()
-        # redundant approach to remove the prohibited edges, but I do this style to keep the route ordering.
-        __seq_edges_updated = [__edge for __edge in __seq_edges if __edge not in seq_prohibited_edges]
-        # logger.debug(f"No: {_v_id}, before-mod-N(edges): {len(__seq_edges)}, updated-N(edges): {len(__seq_edges_updated)}")
-        # updating edge information.
-        __route_attrib['edges'] = " ".join(__seq_edges_updated)
-        elem_route.attrib = __route_attrib
-        
-        # updating stop info
-        elem_stop = elem.find('stop')
-        if elem_stop is not None:
-            _p_id = elem_stop.attrib['parkingArea']
-            if _p_id in seq_parking_id_unavailable:
-                elem.remove(elem_stop)
-                logger.debug(f"removed: {elem_stop}")
-            # end if
+    _id_pedestrian = elem.attrib['id']
+    # updating walk route information
+    elem_walk_def = elem.findall('walk')
+    for _elem_walk in elem_walk_def:
+        # deleting the invalid edges from the route.
+        _seq_edges = _elem_walk.attrib['edges'].split()
+        _seq_edges_updated = [_edge for _edge in _seq_edges if _edge not in seq_prohibited_edges]
+        _elem_walk.attrib['edges'] = " ".join(_seq_edges_updated)
+        if len(_seq_edges_updated) < len(_seq_edges):
+            logger.debug(f"pedestrian: {elem.attrib['id']}, before-mod-N(edges): {len(_seq_edges)}, updated-N(edges): {len(_seq_edges_updated)}")
         # end if
+        
+        # if the edge is empty, I delete the pedestrian.
+        if len(_seq_edges_updated) == 0:
+            is_delete_agent = True
+            logger.debug(f"removed agent: {_id_pedestrian}")
+            continue
+        # end if
+        
+        # if deleted bus-stops is in the route, I delete the bus-stop.
+        if 'busStop' in _elem_walk.attrib and _elem_walk.attrib['busStop'] in seq_bus_stops_deleted:
+            del _elem_walk.attrib['busStop']
+        # end if
+    # end for
+    
+    # check ride information.
+    # If this pedestrian requires a ride that passes through the prohibited edges, I delete the pedestrian.
+    elem_ride_def = elem.findall('ride')
+    for _elem_ride in elem_ride_def:
+        _ride_edge_from = ''
+        _ride_edge_to = ''
+        if 'from' in _elem_ride.attrib:
+            _ride_edge_from = _elem_ride.attrib['from']
+        # end if
+        if 'to' in _elem_ride.attrib:
+            _ride_edge_to = _elem_ride.attrib['to']
+        # end if
+        if _ride_edge_from in seq_prohibited_edges or _ride_edge_to in seq_prohibited_edges:
+            # elem.remove(_elem_ride)
+            # root.remove(elem)
+            is_delete_agent = True
+            logger.debug(f"removed agent: {_id_pedestrian}")
+            continue
+        # end if
+        
+        # bus stop.
+        # If this pedestrian requires a ride that passes through the prohibited edges, I delete the pedestrian.
+        if 'busStop' in _elem_ride.attrib and _elem_ride.attrib['busStop'] in seq_bus_stops_deleted:
+            is_delete_agent = True
+            logger.debug(f"removed agent: {_id_pedestrian}")
+            continue
+        # end if
+    # end for
+    
+    if is_delete_agent:
+        root.remove(elem)
+        continue
+    # end if
+# end if
+    
+iter_vehicle_node = root.findall('vehicle')
+for elem in iter_vehicle_node:
+    elem_route = elem.find('route')
+    assert elem_route is not None, f"not found: route"
+    __route_attrib = elem_route.attrib
+    
+    __seq_edges: ty.List[str] = __route_attrib['edges'].split()
+    # redundant approach to remove the prohibited edges, but I do this style to keep the route ordering.
+    __seq_edges_updated = [__edge for __edge in __seq_edges if __edge not in seq_prohibited_edges]
+    # logger.debug(f"No: {_v_id}, before-mod-N(edges): {len(__seq_edges)}, updated-N(edges): {len(__seq_edges_updated)}")
+    # updating edge information.
+    __route_attrib['edges'] = " ".join(__seq_edges_updated)
+    elem_route.attrib = __route_attrib
+    
+    # updating stop info
+    elem_stop = elem.find('stop')
+    if elem_stop is not None:
+        _p_id = elem_stop.attrib['parkingArea']
+        if _p_id in seq_parking_id_unavailable:
+            elem.remove(elem_stop)
+            logger.debug(f"removed: {elem_stop}")
+        # end if
+    # end if
 # end for
 
 tree_xml.write(t_path_pedestrial_route.as_posix())        
@@ -370,21 +396,22 @@ t_path_special_route = target_path_route_dir / "most.special.rou.xml"
 tree_xml = ET.parse(s_path_special_route.as_posix())
 root = tree_xml.getroot()
 
-for elem in root:
+iter_vehicle_node = root.findall('vehicle')
+
+for elem in iter_vehicle_node:
     # do something with each element here
     element_name: str = elem.tag    
-    if element_name == 'vehicle':
-        elem_route = elem.find('route')
-        assert elem_route is not None, f"not found: route"
-        __route_attrib = elem_route.attrib
-        
-        __seq_edges: ty.List[str] = __route_attrib['edges'].split()
-        # redundant approach to remove the prohibited edges, but I do this style to keep the route ordering.
-        __seq_edges_updated = [__edge for __edge in __seq_edges if __edge not in seq_prohibited_edges]
-        # logger.debug(f"No: {_v_id}, before-mod-N(edges): {len(__seq_edges)}, updated-N(edges): {len(__seq_edges_updated)}")
-        # updating edge information.
-        __route_attrib['edges'] = " ".join(__seq_edges_updated)
-        elem_route.attrib = __route_attrib
+    elem_route = elem.find('route')
+    assert elem_route is not None, f"not found: route"
+    __route_attrib = elem_route.attrib
+    
+    __seq_edges: ty.List[str] = __route_attrib['edges'].split()
+    # redundant approach to remove the prohibited edges, but I do this style to keep the route ordering.
+    __seq_edges_updated = [__edge for __edge in __seq_edges if __edge not in seq_prohibited_edges]
+    # logger.debug(f"No: {_v_id}, before-mod-N(edges): {len(__seq_edges)}, updated-N(edges): {len(__seq_edges_updated)}")
+    # updating edge information.
+    __route_attrib['edges'] = " ".join(__seq_edges_updated)
+    elem_route.attrib = __route_attrib
 # end for
 
 tree_xml.write(t_path_special_route.as_posix())        
